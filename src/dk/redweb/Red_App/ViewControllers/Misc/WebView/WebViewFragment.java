c1203369d4c59beb8968a938479e350f3fc2cca0
Thorbjørn Steen <thorb@redweb.dk>
2013-11-28 13:39:53 +0100

    WebView updated to allow proper use of the back button

--- src/dk/redweb/Red_App/ViewControllers/Misc/WebView/WebViewFragment.java
+++ src/dk/redweb/Red_App/ViewControllers/Misc/WebView/WebViewFragment.java
@@ -1,19 +1,19 @@
 package dk.redweb.Red_App.ViewControllers.Misc.WebView;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebBackForwardList;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.RelativeLayout;
-import dk.redweb.Red_App.AppearanceHelper;
-import dk.redweb.Red_App.MyLog;
-import dk.redweb.Red_App.R;
+import dk.redweb.Red_App.*;
 import dk.redweb.Red_App.StaticNames.DEFAULTTEXT;
 import dk.redweb.Red_App.StaticNames.LOOK;
 import dk.redweb.Red_App.StaticNames.PAGE;
 import dk.redweb.Red_App.StaticNames.TEXT;
-import dk.redweb.Red_App.TextHelper;
 import dk.redweb.Red_App.ViewControllers.BasePageFragment;
 import dk.redweb.Red_App.Views.FlexibleButton;
 import dk.redweb.Red_App.XmlHandling.XmlNode;
@@ -34,11 +34,18 @@ public class WebViewFragment extends BasePageFragment {
         setAppearance();
         setText();
 
-        WebView body = (WebView)findViewById(R.id.webview_webBody);
+        WebView webView = (WebView)findViewById(R.id.webview_webBody);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                return checkBackKey(keyCode, event);
+            }
+        });
 
         try {
             String pageUrl = _page.getStringFromNode(PAGE.URL);
-            body.loadUrl(pageUrl);
+            webView.loadUrl(pageUrl);
         } catch (NoSuchFieldException e) {
             MyLog.e("Exception when attempting to get url for page", e);
         }
@@ -76,6 +83,51 @@ public class WebViewFragment extends BasePageFragment {
             helper.setFlexibleButtonText(R.id.flxBackButton, TEXT.BACKBUTTON, DEFAULTTEXT.BACKBUTTON);
         } catch (Exception e) {
             MyLog.e("Exception when setting text for BaseMapFragment", e);
+        }
+    }
+
+    private boolean checkBackKey(int keyCode, KeyEvent event){
+        if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){
+            goBack();
+            return true;
+        }
+        else if(keyCode == KeyEvent.KEYCODE_BACK){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void setupBackButton(){
+        try {
+            FlexibleButton flxBackButton = (FlexibleButton)findViewById(R.id.flxBackButton);
+            if(_page.hasChild(PAGE.RETURNBUTTON) && _page.getBoolFromNode(PAGE.RETURNBUTTON)){
+                flxBackButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        WebView webView = (WebView)findViewById(R.id.webview_webBody);
+//                        WebBackForwardList list = webView.copyBackForwardList();
+//                        MyLog.v(String.valueOf(list.getSize()) + " " + webView.canGoBack());
+
+                        goBack();
+                    }
+                });
+            } else {
+                flxBackButton.setVisibility(View.GONE);
+            }
+        } catch (NoSuchFieldException e) {
+            MyLog.e("Exception when getting ReturnButton attribute from page xml", e);
+        }
+    }
+
+    private void goBack(){
+        MyLog.v("Back button pressed");
+        WebView webView = (WebView)findViewById(R.id.webview_webBody);
+        if (webView.canGoBack()){
+            webView.goBack();
+        }
+        else{
+            NavController.popPage(getActivity());
         }
     }
 }
